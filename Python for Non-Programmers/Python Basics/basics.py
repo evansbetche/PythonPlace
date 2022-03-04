e76e864233e6d31e5969c4c2b6ebd73d24f571ea
EvansBetChe <cheruiyot.ebett@gmail.com>
2022-03-04 16:55:36 +0300

    Python Basics

--- Python for Non-Programmers/Python Basics/basics.py
+++ Python for Non-Programmers/Python Basics/basics.py
@@ -1,5 +1,6 @@
 print("Hello Evans Bet")
 
+# 1.1 Variables
 # Variables hold a piece of information that can change over time.They can hold numbers , text and can hold other types suchas boolens.
 # The value of the variable can change over time.
 wallet =  5000
@@ -11,6 +12,29 @@ print(wallet)
 wallet = 3000
 print(wallet)
 
+# Make a variable called day and set it equal to the date of the month
+day = 4
+
+# 1.2 Numbers Integer and Floats
+# Learn about Ints and floats
+# Ints is a short form of integer.It means a whole number.A number that does not have a decimal places connected with it.
+# For example 4 above as the value of the variable day. Can be positive or negative number
+# Floats have a decimal places
+# Can do calculations/math
+temp = -15
+ 
+weight = 70.1
+
+print( 4 + 5)
+print(day + 5)
+print(weight * 2)
+
+#Find something around you in your life to represent an int and a float.Put them in a variable
+books = 10
+height = 20.5
+
+# 1.3 Strings
+# Strings are used to represent text inside of python
 
 
 
