90a3329110dafce218c3c21a109139fa1096b2ca
EvansBetChe <cheruiyot.ebett@gmail.com>
2022-03-04 16:52:21 +0300

    Python Basics

--- Python for Non-Programmers/Python Basics/basics.py
+++ Python for Non-Programmers/Python Basics/basics.py
@@ -1,1 +1,16 @@
 print("Hello Evans Bet")
+
+# Variables hold a piece of information that can change over time.They can hold numbers , text and can hold other types suchas boolens.
+# The value of the variable can change over time.
+wallet =  5000
+wallet = 4000
+
+# To show the output of the variables , we use print function to display it.
+print(wallet)
+
+wallet = 3000
+print(wallet)
+
+
+
+
